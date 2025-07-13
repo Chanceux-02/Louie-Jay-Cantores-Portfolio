6c6afd031221faaa5c49363a20d76fe1254964c2
Louie Jay Cantores <99269976+Chanceux-02@users.noreply.github.com>
2025-07-13 14:08:00 +0800

    done about me

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,14 +1,20 @@
 <template>
 <div class="about-container" id="about">
 
-  <div class="about-image">
-    <div class="image" data-aos="zoom-in"></div>
+  <div class="about-image-container">
+    <img :src="myPic" alt="Louie Jay Cantores Picture" class="about-image" data-aos="zoom-in" />
   </div>
 
   <div class="about-text">
     <p class="title" data-aos="zoom-in-down"><span style="color: #F9004D;">About</span> Me</p>
     <p class="about-desc" data-aos="zoom-in">
-      Freelance Shopify developer passionate about crafting seamless e-commerce experiences using Liquid, JavaScript, HTML, and CSS. I specialize in customizing themes, improving site speed, and optimizing for conversions to help businesses turn more visitors into customers. I focus on building responsive, user-friendly stores and enhancing online performance, delivering solutions that elevate brands and drive measurable results.
+      Freelance Shopify developer passionate about crafting seamless e-commerce experiences using Liquid, JavaScript, HTML, and CSS. 
+      <span class="about-desc-extend-text">
+        I specialize in customizing themes, improving site speed, and optimizing for conversions to help businesses turn more visitors into customers.
+      </span>
+      <span class="about-desc-extend-text">
+        I focus on building responsive, user-friendly stores and enhancing online performance, delivering solutions that elevate brands and drive measurable results.
+      </span>
     </p>
   </div>
 
@@ -18,119 +24,119 @@
 
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+  import myPic from '@/assets/myPic/mypic.jpeg'
 </script>
 
 <style scoped>
 
 .about-container{
   display: flex;
-  justify-content: space-evenly;
-  height: 120vh;
-  max-height: 950px;
+  justify-content: space-between;
   background-image: url('@/assets/aboutBG.svg');
   background-size: cover;
   background-position: center;
+  padding: 15% 0;
 }
 
 .about-text{
-  background-color: rgb(25,25,25,0.8);
-  max-width: 60%;
-  padding: 5%;
-  padding-top: 15%;
+  background-color: rgb(25,25,25,0.5);
+  width: 100%;
+  max-width: 85%;
+  margin: auto;
+
 }
 
-.about-image{
+.about-image-container{
     background-color: rgb(25,25,25,0.8);
-    min-height: 200px;
-    width: 50%;
     position: relative;
-}
-.image{
-    background-image: url('@/assets/myPic/mypic.jpeg');
-    background-repeat: no-repeat;
-    height: 60%;
-    width: 90%;
-    background-size: contain;
-    position: absolute;
-    top: 20%;
-    left: 10%;
+    display: none;
 }
 
 .title{
-  font-size: 4rem;
+  font-size: 1.5rem;
 }
+
 .about-desc{
   color: white;
-  line-height: 2; /* Adjust the value as needed */
-  text-align: center;
-  font-size: 1.5rem;
+  line-height: 2; 
+  text-align: justify;
+  font-size: 1rem;
   margin: auto;
+  margin-top: 5%;
+  display: flex;
+  flex-direction: column;
+}
+
+.about-desc-extend-text{
   margin-top: 5%;
 }
 
+@media (min-width: 1024px) {
 
-@media (max-width: 1000px) {
-      .image{
-        background-size:contain;
-      }
-     .title{
-        font-size: 2rem;
-      }
-      .about-desc{
-        font-size: 1rem;
-      }
-      .about-image{
-        display: none;
-      }
-      .about-text {
-        max-width: 100%;
-      }
-      .about-text p{
-        max-width: 80%;
-        margin-right: auto;
-        margin-left: auto;
-      }
-}
-@media (max-width: 768px) {
-    .about-container{
-      height: 60vh;
-    }
-    .about-desc{
-      font-size: 0.8rem;
-    }
-    
-}
-@media (max-width: 425px) {
+  .title{
+    font-size: 3rem;
+  }
 
   .about-container{
+    padding: 5% 10%;
+    gap: 50px;
+  }
+
+  .about-image-container{
     display: block;
-    margin: auto;
+    width: 40%;
+  }
+
+  .about-text{
+    width: 60%;
+  }
+
+  .about-image{
     width: 100%;
-    max-height: 450px;
+    height: 100%;
+    border-radius: 15px;
+    display: block;
   }
-  .image{
-    display: none;
+
+  .about-desc{
+    text-align: left;
   }
+
+}
+
+@media (min-width: 1440px) {
   .title{
+    font-size: 4rem;
+  }
+
+  .about-image{
+    object-position: center;
+    object-fit: contain;
+    max-width: 400px;
+    margin: auto;
+  }
+}
+
+@media (min-width: 1900px) {
+  .about-container{
+    padding: 8% 10%;
+  }
+
+  .title{
+    font-size: 4rem;
+  }
+
+  .about-image{
+    object-position: center;
+    object-fit: contain;
+    max-width: 600px;
+    margin: auto;
+  }
+
+  .about-desc{
     font-size: 2rem;
   }
- 
-  .about-text {
-    background-color: rgb(25, 25, 25, 0.8);
-    max-width: 100%;
-    padding: 15% 0% 0% 0%;
-    height: 100%;
-  }
-  .about-image{
-    padding: 0%;
-    min-height: 0;
-    width: 0%;
-  }
-  
 }
 
 
